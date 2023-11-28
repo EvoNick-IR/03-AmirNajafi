329369b8e1f53b5536848033e80431580f2f0ca9
EvoNick-IR <mostafa.haghani69@Gmail.com>
2023-11-28 16:27:00 +0330

    add : typeScript

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-
+app.get("/", (req, res) => {
+  res.send("hello user");
+});
+app.get("/users", (req, res) => {
+  const users = [
+    { name: "mostafa", lName: "Haghani" },
+    { name: "Hoda", lName: "Ahmadi" },
+  ];
+  res.send(users);
+});
 
 app.listen(process.env.PORT, () =>
   console.log("app is listening to port: " + process.env.PORT)
