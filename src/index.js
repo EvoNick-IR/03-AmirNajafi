69421d500c9a51413246fad68c361041e717b706
EvoNick-IR <mostafa.haghani69@Gmail.com>
2023-11-28 16:03:35 +0330

    added dotenv

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.listen("3000", () => console.log("app is listening to port 3000"));
+
+
+app.listen(process.env.PORT, () =>
+  console.log("app is listening to port: " + process.env.PORT)
+);
